9f86f85f85db195888c3cf834b3b2dccc937c783
Yumjong <96937488+Yumjong@users.noreply.github.com>
2022-03-24 02:50:20 +0900

    navbar scroll javasccript

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 'use strict'
 
+
+// Make navbar opacity when navbar is scrolling
+const navbar = document.querySelector('#navbar');
+const navbarHeight =navbar.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    console.log(window.scrollY)
+    console.log("navbarHeiht",navbarHeight)
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark')
+    }else{
+        navbar.classList.remove('navbar--dark')
+    }
+})
+
 // navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector(".navbar_toggle-btn")
 const navbarMenu =document.querySelector(".navbar_menu")
